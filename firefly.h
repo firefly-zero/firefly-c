d3cf07b1d3d0d72b9ad40456f4736f200313b16c
gram <git@orsinium.dev>
2024-06-14 15:35:41 +0200

    fix import names

--- firefly.h
+++ firefly.h
@@ -2,8 +2,8 @@
 
 #include <stdint.h>
 
-#define WASM_EXPORT(name) __attribute__((export_name(name)))
-#define WASM_IMPORT(name) __attribute__((import_name(name)))
+#define WASM_EXPORT(NAME) __attribute__((export_name(#NAME)))
+#define WASM_IMPORT(MOD, NAME) __attribute__((import_module(MOD), import_name(#NAME)))
 
 #define WIDTH 240
 #define HEIGHT 160
@@ -61,5 +61,5 @@ struct RGB
     int8_t b;
 };
 
-WASM_IMPORT("line")
+WASM_IMPORT("graphics", "draw_line")
 void drawLine(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t color, int32_t stroke_width);
