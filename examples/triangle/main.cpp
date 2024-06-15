84d913b84bbdd98298d9574f14f75fd329a3b70c
gram <git@orsinium.dev>
2024-06-15 10:42:41 +0200

    improve example

--- examples/triangle/main.cpp
+++ examples/triangle/main.cpp
@@ -2,9 +2,10 @@
 
 BOOT void boot()
 {
+    ClearScreen(WHITE);
     DrawTriangle(
         Point{30, 10},
         Point{20, 30},
         Point{40, 30},
-        Style{fill_color : BLACK, stroke_color : RED, stroke_width : 1});
+        Style{fill_color : BLACK, stroke_color : RED, stroke_width : 2});
 }
