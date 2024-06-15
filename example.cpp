2e1f337d9feb28c8e0813baecc69ce530a0cf895
gram <git@orsinium.dev>
2024-06-15 10:32:29 +0200

    restructure

--- example.cpp
+++
@@ -1,6 +0,0 @@
-#include "firefly.c"
-
-BOOT void boot()
-{
-    DrawLine(Point{10, 10}, Point{20, 20}, LineStyle{color : BLACK, width : 1});
-}
---
+++ examples/triangle/main.cpp
@@ -0,0 +1,10 @@
+#include "../../src/firefly.c"
+
+BOOT void boot()
+{
+    DrawTriangle(
+        Point{30, 10},
+        Point{20, 30},
+        Point{40, 30},
+        Style{fill_color : BLACK, stroke_color : RED, stroke_width : 1});
+}
