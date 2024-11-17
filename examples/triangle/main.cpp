3046f69866994122657bd859fdfa27fc8c3e0a33
gram <git@orsinium.dev>
2024-11-17 11:52:26 +0100

    drop custom build commands

--- examples/triangle/main.cpp
+++ examples/triangle/main.cpp
@@ -5,5 +5,9 @@ BOOT void boot()
     ClearScreen(WHITE);
     DrawTriangle(
         {60, 10}, {40, 40}, {80, 40},
-        {.fill_color = LIGHT_GRAY, .stroke_color = DARK_BLUE, .stroke_width = 1});
+        {
+            .fill_color = LIGHT_GRAY,
+            .stroke_color = DARK_BLUE,
+            .stroke_width = 1,
+        });
 }
