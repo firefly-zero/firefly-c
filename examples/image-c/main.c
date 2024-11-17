7e32a2206b4696cc139e16bfd9a21aaad6ea4af8
gram <git@orsinium.dev>
2024-11-17 13:44:58 +0100

    add image example

---
+++ examples/image-c/main.c
@@ -0,0 +1,26 @@
+#include "../../src/firefly.c"
+
+Image image;
+
+BOOT void boot()
+{
+    // We know the image size in advance.
+    // You can see it when running `firefly_cli build`.
+    // You could also use `GetFileSize` to detect the file size
+    // in runtime but that requires using an allocator.
+    const size_t fileSize = 97;
+    Buffer buf;
+    char head[fileSize];
+    buf.head = head;
+    buf.size = fileSize;
+    image = LoadFile("img", buf);
+}
+
+UPDATE void update()
+{
+    ClearScreen(WHITE);
+    Point point;
+    point.x = 10;
+    point.y = 10;
+    DrawImage(image, point);
+}
