03f15f540bb860da4832f2fd926f0a29edcf22d6
gram <git@orsinium.dev>
2026-06-28 15:49:11 +0200

    build docs without apt

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Build script for static docs used on Cloudflare Pages.
 set -e
-which doxygen || apt install -y doxygen
-doxygen
+wget https://www.doxygen.nl/files/doxygen-1.17.0.linux.bin.tar.gz
+tar xf doxygen-*.tar.xz
+rm doxygen-*.tar.xz
+./doxygen-*/bin/doxygen
+
